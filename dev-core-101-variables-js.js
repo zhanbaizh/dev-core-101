5e68602cf5cb018d9aa691a097a6d375778d8303
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-10-08 10:31:00 +0500

    task 12
    js code

---
+++ dev-core-101-variables-js.js
@@ -0,0 +1,9 @@
+let x = 5;
+let y = 10;
+console.log(x * y);  // 75
+console.log(y / x);  // 3
+
+// Работа со строками
+let name = "Owlmasters";
+let greeting = "Hello, " + name;
+console.log(greeting);
