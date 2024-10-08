232ccef655cb46522e50b6ec6918e199ea276fd1
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-10-08 10:41:35 +0500

    udp task 13

--- dev-core-101-input-output-python.py
+++ dev-core-101-input-output-python.py
@@ -2,5 +2,5 @@ from datetime import datetime
 y_name = input('Whas is your name?')
 age = int(input('How old are you?'))
 this_year = datetime.now().year
-print('you will turn 100 in ', 100-age+year)
+print('you will turn 100 in ', 100-age+this_year)
 
