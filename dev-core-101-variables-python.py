2f012067412e2f6454beaa177941c90e336fae17
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-10-08 10:28:31 +0500

    task 12
    python variables

---
+++ dev-core-101-variables-python.py
@@ -0,0 +1,9 @@
+a,b  = int(input(),input())
+if a % 2 == 0:
+    print('This number is even')
+else: 
+    print('This number is odd')
+if a % b == 0:
+    print('b is denumenator for a')
+else:
+    print('b is not denumenator for b')
