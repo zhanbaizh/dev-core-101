55337749cf2d45e3374b617821b60fdfd6443798
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-10-08 20:34:20 +0500

    i have added power
    easy

--- dev-core-101-finalproject.py
+++ dev-core-101-finalproject.py
@@ -16,6 +16,9 @@ def divide(x, y):
         return "Error! Division by zero."
     else:
         return x / y
+#function to perform exponent
+def power(x, y):
+    return pow(x,y)
 
 # Main calculator function
 def calculator():
@@ -25,11 +28,12 @@ def calculator():
     print("2. Subtraction (-)")
     print("3. Multiplication (*)")
     print("4. Division (/)")
+    print("5. Power(^)")
 
     # Get user input for the operation
-    operation = input("Enter the number of the operation (1/2/3/4): ")
+    operation = input("Enter the number of the operation (1/2/3/4/5): ")
 
-    if operation in ['1', '2', '3', '4']:
+    if operation in ['1', '2', '3', '4', '5']:
         # Get user input for numbers
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
@@ -50,6 +54,10 @@ def calculator():
         elif operation == '4':
             result = divide(num1, num2)
             print(f"The result of {num1} / {num2} is: {result}")
+        
+        elif operation == '5':
+            result = power(num1, num2)
+            print(f"The result of {num2} power of {num1} is: {result}")
     else:
         print("Invalid input! Please select a valid operation.")
 
