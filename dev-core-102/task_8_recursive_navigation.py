0a84f16e6107ba720c85d4459feb17b31f69cdb9
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-11-02 23:48:46 +0500

    Update task_8_recursive_navigation.py
    updates on other functions

--- dev-core-102/task_8_recursive_navigation.py
+++ dev-core-102/task_8_recursive_navigation.py
@@ -1,9 +1,32 @@
 import os
 
-def navigate_folder(path, indent=0):
+def print_directory_structure(path, indent=0):
     for item in os.listdir(path):
+        item_path = os.path.join(path, item)
         print("  " * indent + item)
-        if os.path.isdir(os.path.join(path, item)):
-            navigate_folder(os.path.join(path, item), indent + 1)
+        if os.path.isdir(item_path):
+            print_directory_structure(item_path, indent + 1)
 
-navigate_folder('/dev-core-102')
+def search_file(directory, filename):
+    for item in os.listdir(directory):
+        if item == filename:
+            return os.path.join(directory, item)
+        if os.path.isdir(os.path.join(directory, item)):
+            result = search_file(os.path.join(directory, item), filename)
+            if result:
+                return result
+    return None
+
+def calculate_total_size(directory):
+    total_size = 0
+    for item in os.listdir(directory):
+        path = os.path.join(directory, item)
+        if os.path.isfile(path):
+            total_size += os.path.getsize(path)
+        elif os.path.isdir(path):
+            total_size += calculate_total_size(path)
+    return total_size
+
+print_directory_structure('/Users/zhandosbaizhuma/documents/GitHub')
+search_file('/Users/zhandosbaizhuma/documents/GitHub', 'root')
+calculate_total_size('/Users/zhandosbaizhuma/documents/GitHub')
